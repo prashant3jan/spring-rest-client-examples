533ef0facf8d0235f7e76f5a5b855f2b9b60af17
prashant3jan <prashant3jan@gmail.com>
2022-12-31 16:50:02 +0530

    adding URI Component Builder

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,12 +1,17 @@
 package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.springrestclientexamples.domain.User;
+import guru.springframework.springrestclientexamples.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ApiServiceImpl implements ApiService{
     /**
@@ -14,20 +19,26 @@ public class ApiServiceImpl implements ApiService{
      * @return
      */
     private RestTemplate restTemplate;
+    private String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url=api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        List<User> userData = new ArrayList<>();
-        User[] userArray = Arrays.stream(restTemplate.getForObject("http://localhost:3000/users", User[].class)).limit(limit).toArray(User[]::new);
-        userData= Arrays.asList(userArray);
-        for(int i=0; i<userData.size(); i++){
-            System.out.println(userData.get(i));
-            System.out.println("gender"+userData.get(i).getGender());
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url);
+        UserData userData = new UserData();
+        User[] userArray = Arrays.stream(restTemplate.getForObject(uriBuilder.toUriString(), User[].class)).limit(limit).toArray(User[]::new);
+        userData.setData(Arrays.asList(userArray));
+        for(int i=0; i<userData.getData().size(); i++){
+            System.out.println(userData.getData().get(i));
+            System.out.println("gender"+userData.getData().get(i).getGender());
         }
-        return userData;
+        return userData.getData();
+
     }
 }
